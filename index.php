00597c29620d4123f6b044aff0e624d054d03fa2
Ana Paula Olavarrieta <anapolavarrieta@gmail.com>
2015-05-22 12:20:54 -0500

    Changes to index first page

--- index.php
+++ index.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Idesign</title>
-</head>
 
-<body id="part1">
-	<div class="inicio">
 		<?php include("php/index.php"); ?>
 	
-	</div>
-</body>  
- 
-</html>
