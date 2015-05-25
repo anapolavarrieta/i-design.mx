9f2cc255220060234887f1041dedcb3291f69fea
Ana Paula Olavarrieta <anapolavarrieta@gmail.com>
2015-05-24 22:18:11 -0500

    Adding all images

--- php/novias.php
+++ php/novias.php
@@ -19,11 +19,16 @@
 	</div>
 			
 	<div id="links">
-		<a href="../Images/gallery/novia1.jpg" data-gallery> <img src="../Images/gallery/th/novia1_th.bmp" alt="Novia1"></a>
-		<a href="../Images/gallery/novia2.jpg" data-gallery> <img src="../Images/gallery/th/novia2_th.bmp" alt="Novia2"></a>
-		<a href="../Images/gallery/novia3.jpg" data-gallery> <img src="../Images/gallery/th/novia3_th.bmp" alt="Novia3"></a>
-		<a href="../Images/gallery/novia4.jpg" data-gallery> <img src="../Images/gallery/th/novia4_th.bmp" alt="Novia4"></a>
+		<a href="../Images/gallery/novia1.jpg" data-gallery> <img src="../Images/gallery/th/tn_novia1.jpg" alt="Novia1"></a>
+		<a href="../Images/gallery/novia2.jpg" data-gallery> <img src="../Images/gallery/th/tn_novia2.jpg" alt="Novia2"></a>
+		<a href="../Images/gallery/novia3.jpg" data-gallery> <img src="../Images/gallery/th/tn_novia3.jpg" alt="Novia3"></a>
+		<a href="../Images/gallery/novia4.jpg" data-gallery> <img src="../Images/gallery/th/tn_novia4.jpg" alt="Novia4"></a>
 		<a href="../Images/gallery/novia5.jpg" data-gallery> <img src="../Images/gallery/th/tn_novia5.jpg" alt="Novia5"></a>
+		<a href="../Images/gallery/novia6.jpg" data-gallery> <img src="../Images/gallery/th/tn_novia6.jpg" alt="Novia5"></a>
+		<a href="../Images/gallery/novia7.jpg" data-gallery> <img src="../Images/gallery/th/tn_novia7.jpg" alt="Novia5"></a>
+		<a href="../Images/gallery/novia8.jpg" data-gallery> <img src="../Images/gallery/th/tn_novia8.jpg" alt="Novia5"></a>
+		<a href="../Images/gallery/novia9.jpg" data-gallery> <img src="../Images/gallery/th/tn_novia9.jpg" alt="Novia5"></a>
+		<a href="../Images/gallery/novia10.jpg" data-gallery> <img src="../Images/gallery/th/tn_novia10.jpg" alt="Novia5"></a>
 	</div>
 
 </body>
